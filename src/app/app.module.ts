1fb3faee7a7a7600db8f9dbba2509ea019879420
Prahalad Belavadi <prahalad@belavadi.com>
2019-06-09 22:43:30 +0530

    add material and header and footer
    Signed-off-by: Prahalad Belavadi <prahalad@belavadi.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+import {MatToolbarModule, MatIconModule} from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
