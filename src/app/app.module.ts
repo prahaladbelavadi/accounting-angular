eec38d0038777cc8af3b28dda641d2a4b89e083c
Prahalad Belavadi <prahalad@belavadi.com>
2019-06-13 00:48:23 +0530

    strip some login functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,46 +9,6 @@ import { FooterComponent } from './footer/footer.component';
 
 import { MatToolbarModule, MatIconModule, MatButtonModule } from '@angular/material';
 
-import {
-  SocialLoginModule,
-  AuthServiceConfig,
-  GoogleLoginProvider,
-} from 'angular-6-social-login-v2';
-
-
-
-// Configs
-export function getAuthServiceConfigs() {
-  const config = new AuthServiceConfig(
-    [
-      {
-        id: GoogleLoginProvider.PROVIDER_ID,
-        provider: new GoogleLoginProvider('496119684571-gfb9gmpm4ja5bj7gglfjbsu8650d4jkr.apps.googleusercontent.com')
-      }
-    ]
-  )
-  return config;
-}
-
-
-// Configs
-export function getAuthServiceConfigs() {
-  let config = new AuthServiceConfig(
-      [
-        {
-          id: FacebookLoginProvider.PROVIDER_ID,
-	      provider: new FacebookLoginProvider("Your-Facebook-app-id")
-        },
-        {
-          id: GoogleLoginProvider.PROVIDER_ID,
-	      provider: new GoogleLoginProvider("Your-Google-Client-Id")
-        }
-      ];
-  );
-  return config;
-}
-
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -59,16 +19,12 @@ export function getAuthServiceConfigs() {
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    SocialLoginModule,
     MatToolbarModule,
     MatIconModule,
     MatIconModule,
     MatButtonModule
   ],
-  providers: [{
-    provide:  AuthServiceConfig,
-    useFactory: getAuthServiceConfigs
-  }],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-import {
-  AuthService,
-  GoogleLoginProvider,
-} from 'angular-6-social-login-v2';
-
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -12,23 +7,7 @@ import {
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(private socialAuthService: AuthService) { }
-
-
-  public socialSignIn(socialPlatform: string) {
-    let socialPlatformProvider;
-    if (socialPlatform === 'google') {
-      socialPlatformProvider = GoogleLoginProvider.PROVIDER_ID;
-    }
-
-    this.socialAuthService.signIn(socialPlatformProvider).then(
-      (userData) => {
-        console.log(userData);
-
-      }
-    );
-    }
-
+  constructor() { }
 
   ngOnInit() {
   }
