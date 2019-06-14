8d86d0d658d90a358b28ecdacc7b22377ffaf74b
Prahalad Belavadi <prahalad@belavadi.com>
2019-06-15 02:13:28 +0530

    modify to gender and number of hours worked

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -5,21 +5,21 @@ import { MatTableDataSource, MatSort } from '@angular/material';
 export interface PeriodicElement {
   name: string;
   position: number;
-  weight: number;
-  symbol: string;
+  hours: number;
+  gender: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  { position: 1, name: 'Hydrogen', hours: 1.0079, gender: 'H' },
+  { position: 2, name: 'Helium', hours: 4.0026, gender: 'He' },
+  { position: 3, name: 'Lithium', hours: 6.941, gender: 'Li' },
+  { position: 4, name: 'Beryllium', hours: 9.0122, gender: 'Be' },
+  { position: 5, name: 'Boron', hours: 10.811, gender: 'B' },
+  { position: 6, name: 'Carbon', hours: 12.0107, gender: 'C' },
+  { position: 7, name: 'Nitrogen', hours: 14.0067, gender: 'N' },
+  { position: 8, name: 'Oxygen', hours: 15.9994, gender: 'O' },
+  { position: 9, name: 'Fluorine', hours: 18.9984, gender: 'F' },
+  { position: 10, name: 'Neon', hours: 20.1797, gender: 'Ne' },
 ];
 
 @Component({
@@ -30,14 +30,13 @@ const ELEMENT_DATA: PeriodicElement[] = [
 
 export class DashboardComponent implements OnInit {
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['position', 'name', 'hours', 'gender'];
   dataSource = new MatTableDataSource(ELEMENT_DATA);
 
-  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
 
   ngOnInit() {
     this.dataSource.sort = this.sort;
   }
-
 }
 
