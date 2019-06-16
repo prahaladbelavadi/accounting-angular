c93cd6098359bd5437fca4455d1edfe88aef1f20
Prahalad Belavadi <prahalad@belavadi.com>
2019-06-17 00:03:57 +0530

    remove gender and place role; jobs are gender neutral
    Signed-off-by: Prahalad Belavadi <prahalad@belavadi.com>

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,20 +6,20 @@ export interface PeriodicElement {
   name: string;
   position: number;
   hours: number;
-  gender: string;
+  role: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', hours: 1.0079, gender: 'H' },
-  { position: 2, name: 'Helium', hours: 4.0026, gender: 'He' },
-  { position: 3, name: 'Lithium', hours: 6.941, gender: 'Li' },
-  { position: 4, name: 'Beryllium', hours: 9.0122, gender: 'Be' },
-  { position: 5, name: 'Boron', hours: 10.811, gender: 'B' },
-  { position: 6, name: 'Carbon', hours: 12.0107, gender: 'C' },
-  { position: 7, name: 'Nitrogen', hours: 14.0067, gender: 'N' },
-  { position: 8, name: 'Oxygen', hours: 15.9994, gender: 'O' },
-  { position: 9, name: 'Fluorine', hours: 18.9984, gender: 'F' },
-  { position: 10, name: 'Neon', hours: 20.1797, gender: 'Ne' },
+  { position: 1, name: 'Hydrogen', hours: 1.0079, role: 'H' },
+  { position: 2, name: 'Helium', hours: 4.0026, role: 'He' },
+  { position: 3, name: 'Lithium', hours: 6.941, role: 'Li' },
+  { position: 4, name: 'Beryllium', hours: 9.0122, role: 'Be' },
+  { position: 5, name: 'Boron', hours: 10.811, role: 'B' },
+  { position: 6, name: 'Carbon', hours: 12.0107, role: 'C' },
+  { position: 7, name: 'Nitrogen', hours: 14.0067, role: 'N' },
+  { position: 8, name: 'Oxygen', hours: 15.9994, role: 'O' },
+  { position: 9, name: 'Fluorine', hours: 18.9984, role: 'F' },
+  { position: 10, name: 'Neon', hours: 20.1797, role: 'Ne' },
 ];
 
 @Component({
@@ -30,7 +30,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 
 export class DashboardComponent implements OnInit {
 
-  displayedColumns: string[] = ['position', 'name', 'hours', 'gender'];
+  displayedColumns: string[] = ['position', 'name', 'hours', 'role'];
   dataSource = new MatTableDataSource(ELEMENT_DATA);
 
   @ViewChild(MatSort, { static: true }) sort: MatSort;
