ce9fcfa11c52a92b50fdb45f19dd52c0afadd5f6
Prahalad Belavadi <prahalad@belavadi.com>
2019-06-17 00:02:31 +0530

    customize for role and name only
    Signed-off-by: Prahalad Belavadi <prahalad@belavadi.com>

--- src/app/workforce/workforce-attendance/workforce-attendance.component.ts
+++ src/app/workforce/workforce-attendance/workforce-attendance.component.ts
@@ -11,7 +11,7 @@ import { SelectionModel } from '@angular/cdk/collections';
 export class WorkforceAttendanceComponent implements OnInit {
   date = Date.now()
 
-  displayedColumns: string[] = ['select', 'position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['select', 'name', 'role'];
   dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
   selection = new SelectionModel<PeriodicElement>(true, []);
 
@@ -25,8 +25,8 @@ export class WorkforceAttendanceComponent implements OnInit {
   /** Selects all rows if they are not all selected; otherwise clear selection. */
   masterToggle() {
     this.isAllSelected() ?
-        this.selection.clear() :
-        this.dataSource.data.forEach(row => this.selection.select(row));
+      this.selection.clear() :
+      this.dataSource.data.forEach(row => this.selection.select(row));
   }
 
   /** The label for the checkbox on the passed row */
@@ -46,20 +46,38 @@ export class WorkforceAttendanceComponent implements OnInit {
 
 export interface PeriodicElement {
   name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  role: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  { name: 'Hydrogen', role: 'H' },
+  { name: 'Helium', role: 'He' },
+  { name: 'Lithium', role: 'Li' },
+  { name: 'Beryllium', role: 'Be' },
+  { name: 'Boron', role: 'B' },
+  { name: 'Carbon', role: 'C' },
+  { name: 'Nitrogen', role: 'N' },
+  { name: 'Oxygen', role: 'O' },
+  { name: 'Fluorine', role: 'F' },
+  { name: 'Neon', role: 'Ne' },
+  { name: 'Hydrogen', role: 'H' },
+  { name: 'Helium', role: 'He' },
+  { name: 'Lithium', role: 'Li' },
+  { name: 'Beryllium', role: 'Be' },
+  { name: 'Boron', role: 'B' },
+  { name: 'Carbon', role: 'C' },
+  { name: 'Nitrogen', role: 'N' },
+  { name: 'Oxygen', role: 'O' },
+  { name: 'Fluorine', role: 'F' },
+  { name: 'Neon', role: 'Ne' },
+  { name: 'Hydrogen', role: 'H' },
+  { name: 'Helium', role: 'He' },
+  { name: 'Lithium', role: 'Li' },
+  { name: 'Beryllium', role: 'Be' },
+  { name: 'Boron', role: 'B' },
+  { name: 'Carbon', role: 'C' },
+  { name: 'Nitrogen', role: 'N' },
+  { name: 'Oxygen', role: 'O' },
+  { name: 'Fluorine', role: 'F' },
+  { name: 'Neon', role: 'Ne' },
 ];
